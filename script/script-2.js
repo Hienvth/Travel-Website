b228a8e4d3aba68aed8bd670b003426631be4168
Hienvth <80890913+Hienvth@users.noreply.github.com>
2022-09-11 20:48:16 +0700

    update galleryPage and add blogPage

---
+++ script/script-2.js
@@ -0,0 +1,9 @@
+let video = document.querySelector('.video-wrapper video');
+document.getElementById('play-btn').addEventListener('click', ()=> {
+    if (video.paused) {
+        video.play();
+    }
+    else {
+        video.pause();
+    }
+})
