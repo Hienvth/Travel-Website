f42b167b29e040e408d45b619487c8a08c8c8270
Hienvth <80890913+Hienvth@users.noreply.github.com>
2022-10-11 08:50:30 +0700

    practice: website rebuild with html-scss-js

--- script/script-2.js
+++
@@ -1,9 +0,0 @@
-let video = document.querySelector('.video-wrapper video');
-document.getElementById('play-btn').addEventListener('click', ()=> {
-    if (video.paused) {
-        video.play();
-    }
-    else {
-        video.pause();
-    }
-})
